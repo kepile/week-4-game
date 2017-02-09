bdadf09c98cfbb615fa23e98bb6508f55718ce2d
Kathy <kepile@verizon.net>
2017-02-09 13:29:56 -0500

    changes to html, css, js and images

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var targetNumber = 53;
+
+$("#number-to-guess").text(targetNumber);
+
+var counter = 0;
+var numberOptions = [10, 5, 3, 7];
+ for (var i = 0; i < numberOptions.length; i++) {
+   imageCrystal.addClass("crystal-image");
+    var imageCrystal = $("<>");
